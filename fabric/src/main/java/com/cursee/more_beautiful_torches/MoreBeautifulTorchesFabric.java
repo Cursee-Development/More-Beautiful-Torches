2dc18a5abd7ae9e6ec54e67fd8dab778c107a6b1
jason13official <jason13official@gmail.com>
2024-06-25 15:18:32 -0400

    recipes -> recipe and removed unused old

--- fabric/src/main/java/com/cursee/more_beautiful_torches/MoreBeautifulTorchesFabric.java
+++ fabric/src/main/java/com/cursee/more_beautiful_torches/MoreBeautifulTorchesFabric.java
@@ -1,6 +1,8 @@
 package com.cursee.more_beautiful_torches;
 
 import com.cursee.monolib.core.sailing.Sailing;
+import com.cursee.more_beautiful_torches.core.ModBlocksFabric;
+import com.cursee.more_beautiful_torches.core.ModCreativeModeTabFabric;
 import net.fabricmc.api.ModInitializer;
 
 public class MoreBeautifulTorchesFabric implements ModInitializer {
@@ -10,5 +12,8 @@ public class MoreBeautifulTorchesFabric implements ModInitializer {
         
         MoreBeautifulTorches.init();
         Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
+
+        ModBlocksFabric.register();
+        ModCreativeModeTabFabric.register();
     }
 }
